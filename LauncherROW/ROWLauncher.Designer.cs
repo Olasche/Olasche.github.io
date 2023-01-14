da97cd909a7fc230e1ec560103c0ba39c24de97a
Arthwin <dan_olarte@hotmail.com>
2023-01-14 17:27:02 -0500

    First Commit

---
+++ LauncherROW/ROWLauncher.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace LauncherROW
+{
+    partial class ROWLauncher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ROWLauncher));
+            this.background = new System.Windows.Forms.PictureBox();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.MinimizeButton = new System.Windows.Forms.Button();
+            this.PlayButton = new System.Windows.Forms.Button();
+            this.VersionText = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.background)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // background
+            // 
+            this.background.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.background.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.background.Image = ((System.Drawing.Image)(resources.GetObject("background.Image")));
+            this.background.Location = new System.Drawing.Point(16, 25);
+            this.background.Name = "background";
+            this.background.Size = new System.Drawing.Size(400, 400);
+            this.background.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.background.TabIndex = 0;
+            this.background.TabStop = false;
+            this.background.MouseDown += new System.Windows.Forms.MouseEventHandler(this.background_MouseDown);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.BackColor = System.Drawing.Color.Crimson;
+            this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CloseButton.ForeColor = System.Drawing.Color.Gold;
+            this.CloseButton.Location = new System.Drawing.Point(372, 12);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(30, 30);
+            this.CloseButton.TabIndex = 1;
+            this.CloseButton.Text = "X";
+            this.CloseButton.UseVisualStyleBackColor = false;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // MinimizeButton
+            // 
+            this.MinimizeButton.BackColor = System.Drawing.Color.Crimson;
+            this.MinimizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MinimizeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinimizeButton.ForeColor = System.Drawing.Color.Gold;
+            this.MinimizeButton.Location = new System.Drawing.Point(336, 12);
+            this.MinimizeButton.Name = "MinimizeButton";
+            this.MinimizeButton.Size = new System.Drawing.Size(30, 30);
+            this.MinimizeButton.TabIndex = 2;
+            this.MinimizeButton.Text = "-";
+            this.MinimizeButton.UseVisualStyleBackColor = false;
+            this.MinimizeButton.Click += new System.EventHandler(this.MinimizeButton_Click);
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.BackColor = System.Drawing.Color.Crimson;
+            this.PlayButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.PlayButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayButton.ForeColor = System.Drawing.Color.Gold;
+            this.PlayButton.Location = new System.Drawing.Point(113, 455);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(200, 40);
+            this.PlayButton.TabIndex = 3;
+            this.PlayButton.Text = "Checking for Updates";
+            this.PlayButton.UseVisualStyleBackColor = false;
+            this.PlayButton.Click += new System.EventHandler(this.PlayButton_Click);
+            // 
+            // VersionText
+            // 
+            this.VersionText.AutoSize = true;
+            this.VersionText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VersionText.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.VersionText.Location = new System.Drawing.Point(372, 508);
+            this.VersionText.Name = "VersionText";
+            this.VersionText.Size = new System.Drawing.Size(45, 17);
+            this.VersionText.TabIndex = 4;
+            this.VersionText.Text = "0.0.0";
+            // 
+            // ROWLauncher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(434, 541);
+            this.ControlBox = false;
+            this.Controls.Add(this.VersionText);
+            this.Controls.Add(this.PlayButton);
+            this.Controls.Add(this.MinimizeButton);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.background);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ROWLauncher";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ROWLauncher";
+            this.Load += new System.EventHandler(this.ROWLauncher_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ROWLauncher_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.background)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox background;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Button MinimizeButton;
+        private System.Windows.Forms.Button PlayButton;
+        private System.Windows.Forms.Label VersionText;
+    }
+}
+
---
+++ LauncherROW/ROWLauncher.cs
@@ -0,0 +1,268 @@
+﻿using SevenZipExtractor;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace LauncherROW
+{
+    enum LauncherStatus
+    {
+        ready,
+        failed,
+        downloadingGame,
+        downloadingUpdate
+    }
+
+    public partial class ROWLauncher : Form
+    {
+        private static readonly string version_file_link = "https://drive.google.com/uc?export=download&id=1Pst2I1aOIeB8232pYD6nV7u2uE72mqVf";
+        private static readonly string game_zip_link = "https://drive.google.com/uc?export=download&id=12KKDjbVDjMM-5e6fL1OfeEoLouxbzgnb";
+        private static readonly string builds_folder_name = "Builds";
+        private static readonly string executable_name = "ROW";
+
+        private string rootPath;
+        private string versionFile;
+        private string gameZip;
+        private string gameExe;
+
+        private LauncherStatus _status;
+        internal LauncherStatus Status
+        {
+            get => _status;
+            set
+            {
+                _status = value;
+                switch (_status)
+                {
+                    case LauncherStatus.ready:
+                        PlayButton.Text = "Play";
+                        break;
+                    case LauncherStatus.failed:
+                        PlayButton.Text = "Update Failed - Retry";
+                        break;
+                    case LauncherStatus.downloadingGame:
+                        PlayButton.Text = "Downloading Game";
+                        break;
+                    case LauncherStatus.downloadingUpdate:
+                        PlayButton.Text = "Downloading Update";
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        public ROWLauncher()
+        {
+            InitializeComponent();
+            rootPath = Directory.GetCurrentDirectory();
+            versionFile = Path.Combine(rootPath, "Version.txt");
+            gameZip = Path.Combine(rootPath, builds_folder_name + ".rar");
+            gameExe = Path.Combine(rootPath, builds_folder_name, executable_name + ".exe");
+        }
+
+        private void CheckForUpdates()
+        {
+            PlayButton.Enabled = false;
+            if (File.Exists(versionFile))
+            {
+                Version localVersion = new Version(File.ReadAllText(versionFile));
+                VersionText.Text = localVersion.ToString();
+
+                try
+                {
+                    WebClient webClient = new WebClient();
+                    Version onlineVersion = new Version(webClient.DownloadString(version_file_link));
+
+                    if (onlineVersion.IsDifferentThan(localVersion))
+                    {
+                        InstallGameFiles(true, onlineVersion);
+                    }
+                    else
+                    {
+                        Status = LauncherStatus.ready;
+                        PlayButton.Enabled = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Status = LauncherStatus.failed;
+                    PlayButton.Enabled = true;
+                    MessageBox.Show($"Error checking for game updates: {ex}");
+                }
+            }
+            else
+            {
+                InstallGameFiles(false, Version.zero);
+            }
+        }
+
+        private void InstallGameFiles(bool _isUpdate, Version _onlineVersion)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                if (_isUpdate)
+                {
+                    Status = LauncherStatus.downloadingUpdate;
+                }
+                else
+                {
+                    Status = LauncherStatus.downloadingGame;
+                    _onlineVersion = new Version(webClient.DownloadString(version_file_link));
+                }
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadGameCompletedCallback);
+                webClient.DownloadFileAsync(new Uri(game_zip_link), gameZip, _onlineVersion);
+            }
+            catch (Exception ex)
+            {
+                Status = LauncherStatus.failed;
+                PlayButton.Enabled = true;
+                MessageBox.Show($"Error installing game files: {ex}");
+            }
+        }
+
+        private void DownloadGameCompletedCallback(object sender, AsyncCompletedEventArgs e)
+        {
+            try
+            {
+                string onlineVersion = ((Version)e.UserState).ToString();
+
+                using (ArchiveFile archiveFile = new ArchiveFile(gameZip))
+                {
+                    archiveFile.Extract(rootPath);
+                }
+
+                File.Delete(gameZip);
+
+                File.WriteAllText(versionFile, onlineVersion);
+
+                VersionText.Text = onlineVersion;
+                Status = LauncherStatus.ready;
+                PlayButton.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                Status = LauncherStatus.failed;
+                PlayButton.Enabled = true;
+                MessageBox.Show($"Error finishing download: {ex}");
+            }
+        }
+        private void ROWLauncher_Load(object sender, EventArgs e)
+        {
+            CloseButton.BackColor = ColorTranslator.FromHtml("#FF990033");
+            CloseButton.ForeColor = ColorTranslator.FromHtml("#FFFFB066");
+            CloseButton.FlatAppearance.BorderColor = ColorTranslator.FromHtml("#FFFFB066");
+            CloseButton.FlatAppearance.BorderSize = 2;
+            MinimizeButton.BackColor = ColorTranslator.FromHtml("#FF990033");
+            MinimizeButton.ForeColor = ColorTranslator.FromHtml("#FFFFB066");
+            MinimizeButton.FlatAppearance.BorderColor = ColorTranslator.FromHtml("#FFFFB066");
+            MinimizeButton.FlatAppearance.BorderSize = 2;
+            PlayButton.BackColor = ColorTranslator.FromHtml("#FF990033");
+            PlayButton.ForeColor = ColorTranslator.FromHtml("#FFFFB066");
+            PlayButton.FlatAppearance.BorderColor = ColorTranslator.FromHtml("#FFFFB066");
+            PlayButton.FlatAppearance.BorderSize = 2;
+            CheckForUpdates();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, ClientRectangle, ColorTranslator.FromHtml("#FFFFB066"), ButtonBorderStyle.Solid);
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void MinimizeButton_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        private void ROWLauncher_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void background_MouseDown(object sender, MouseEventArgs e)
+        {
+            ROWLauncher_MouseDown(sender, e);
+        }
+
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(gameExe) && Status == LauncherStatus.ready)
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo(gameExe);
+                startInfo.WorkingDirectory = Path.Combine(rootPath, builds_folder_name);
+                Process.Start(startInfo);
+
+                Close();
+            }
+            else if (Status == LauncherStatus.failed)
+            {
+                CheckForUpdates();
+            }
+        }
+    }
+
+    struct Version
+    {
+        internal static Version zero = new Version(0, 0, 0);
+
+        private short major;
+        private short minor;
+        private short subMinor;
+
+        internal Version(short _major, short _minor, short _subMinor)
+        {
+            major = _major;
+            minor = _major;
+            subMinor = _subMinor;
+        }
+        internal Version(string _version)
+        {
+            string[] _versionString = _version.Split('.');
+            if (_versionString.Length != 3)
+            {
+                major = 0;
+                minor = 0;
+                subMinor = 0;
+                return;
+            }
+            major = short.Parse(_versionString[0]);
+            minor = short.Parse(_versionString[1]);
+            subMinor = short.Parse(_versionString[2]);
+        }
+
+        internal bool IsDifferentThan(Version _otherVersion)
+        {
+            if (major != _otherVersion.major)
+                return true;
+            else if (minor != _otherVersion.minor)
+                return true;
+            else if (subMinor != _otherVersion.subMinor)
+                return true;
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"{major}.{minor}.{subMinor}";
+        }
+    }
+}
